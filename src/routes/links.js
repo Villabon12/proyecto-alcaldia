ad87261ff6e8f606bf380a44b11663a2112f1a4f
DanielAviiles <srhackme@gmail.com>
2020-08-30 11:38:58 -0500

    Actualizacion en formularios y estilos

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,8 +5,13 @@ const router = express.Router();
 router.get('/formulariohve', (req, res) => {
   res.render('links/formHVE', {titleForm: 'HOJA DE VIDA DEL EQUIPO'});
 });
+
 router.get('/formulariomde', (req, res) => {
   res.render('links/formMDE', {titleForm: 'MANTENIMIENTO DE EQUIPOS'});
+});
+
+router.get('/formulariofte', (req, res) => {
+  res.render('links/formFTE', {titleForm: 'FICHA TECNICA DEL EQUIPO'});
 });
 
 module.exports = router;
